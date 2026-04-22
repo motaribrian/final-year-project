8272bf36851106b90158b7d8a4443415f3ecf3aa
motaribrian <brianmotari02@gmail.com>
2026-04-22 09:50:24 +0300

    added refgister form

--- src/main/java/com/codewithmotari/collegetimetabling/dto/Registerdto.java
+++ src/main/java/com/codewithmotari/collegetimetabling/dto/Registerdto.java
@@ -1,4 +1,81 @@
 package com.codewithmotari.collegetimetabling.dto;
 
+import com.codewithmotari.collegetimetabling.domain.ROLE;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
 public class Registerdto {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+
+    private String repeatPassword;
+
+    @NotEmpty(message = "At least one role must be selected")
+    private List<ROLE> userRoles;
+
+    public Registerdto() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public List<ROLE> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<ROLE> userRoles) {
+        this.userRoles = userRoles;
+    }
 }
--- src/main/java/com/codewithmotari/collegetimetabling/persistence/TeacherRepository.java
+++ src/main/java/com/codewithmotari/collegetimetabling/persistence/TeacherRepository.java
@@ -1,4 +1,7 @@
 package com.codewithmotari.collegetimetabling.persistence;
 
-public interface TeacherRepository {
+import com.codewithmotari.collegetimetabling.domain.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher,Long> {
 }
